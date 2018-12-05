693997e1138741aecab4419d1633fda14c5132aa
Avazkhon <kamalxanovavazxon@gmail.com>
2018-12-05 19:01:07 +0700

    req.body

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
 const express = require ('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// app.use(bodyParser.json());
+// app.use(bodyParser.json({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
+
 let port = 2019;
 
-app.use(express.static('public'));
+const personal = []
 
+app.use(express.static('public'));
+	// Гоавная страница
 app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/public/main.html');
+});
+
+app.get('/index.html', (req, res) => {
 	res.sendFile(__dirname + '/public/index.html');
+})
+
+	// Список сотрудников
+app.get('/personalLIst.html', (req, res) => {
+	res.sendFile(__dirname + '/public/personalLIst.html');
+	res.send(personal);
+})
+
+	// Новый сотрудник
+app.post('/form.html', function(req, res) {
+	let user = {
+		id: Math.floor(Math.random() * (9999, 9999999)) + 1,
+		user: req.body
+	}
+	res.sendFile(__dirname + '/public/personalLIst.html');
+	personal.push(user);
+	console.log(personal);
+
 });
 
 app.listen(port, () => {
-	console.log(`app starting. localhost:${port}`)
+	console.log(`app starting. localhost:${port}`);
 });
--- public/javaScript/main.js
+++ public/javaScript/main.js
@@ -25,7 +25,7 @@ function newScils () {
 	// если стока не пуста
 	if(scil !== '') {
 		// то отобразить 
-		document.getElementById('listScils').innerHTML += `<li class='liscil'>${scil}</li>`;
+		document.getElementById('listScils').innerHTML += `<li class='liscil' name='${scil}'>${scil}</li>`;
 		// удалить содержимое
 		document.getElementById('scil').value = '';
 		obj.scils.push(scil);
@@ -37,49 +37,47 @@ function Languages () {
 	let newLanguages = document.getElementById('newLanguages').value;
 
 	if(newLanguages !== '') {
-		document.getElementById('listLangulares').innerHTML += `<li class='listLangulares'>${newLanguages}</li>`;
+		document.getElementById('listLangulares').innerHTML += `<li class='listLangulares' name='${newLanguages}' >${newLanguages}</li>`;
 		document.getElementById('newLanguages').value = '';
 		obj.languages.push(newLanguages)
 	}
 }
 
-// сбор данных и ее фикцация
-function personalNew() {
-	let newUser = document.getElementById("newUser");
-		// сбор данных из html
-	let firstName = document.getElementById('firstName');
-	let lastName = document.getElementById('lastName');
-	let familyName = document.getElementById('familyName');
-	let age = document.getElementById('age');
-	let profession = document.getElementById('profession');
-	let education = document.getElementById('education');
-	let institutionName = document.getElementById('institutionName');
-	let position = document.getElementById('position');
-	let wage = document.getElementById('wage');
-	let subdivision = document.getElementById('subdivision');
-	let lavel = document.getElementById('lavel');
-	let listScils = document.getElementById('listScils');
-	let listLangulares = document.getElementById('listLangulares');
+// // сбор данных и ее фикцация
+// function personalNew() {
+// 	let newUser = document.getElementById("newUser");
+// 		// сбор данных из html
+// 	let firstName = document.getElementById('firstName');
+// 	let lastName = document.getElementById('lastName');
+// 	let familyName = document.getElementById('familyName');
+// 	let age = document.getElementById('age');
+// 	let profession = document.getElementById('profession');
+// 	let education = document.getElementById('education');
+// 	let institutionName = document.getElementById('institutionName');
+// 	let position = document.getElementById('position');
+// 	let wage = document.getElementById('wage');
+// 	let subdivision = document.getElementById('subdivision');
+// 	let lavel = document.getElementById('lavel');
+// 	let listScils = document.getElementById('listScils');
+// 	let listLangulares = document.getElementById('listLangulares');
 
-	let id = Math.floor(Math.random() * (99999999 - 0 + 1)) + 9998
+// 	let id = Math.floor(Math.random() * (99999999 - 0 + 1)) + 9998
 
-	// добавления в глобальный массив
-	personal.push({
-		id: id,
-		firstName: firstName.value,
-		lastName: lastName.value,
-		familyName: familyName.value,
-		age: age.value,
-		profession: profession.value,
-		education: education.value,
-		institutionName: institutionName.value,
-		position: position.value,
-		wage: wage.value,
-		subdivision: subdivision.value,
-		lavel: lavel.value,
-		listScils : obj.scils,
-		listLangulares : obj.languages
-	})
-	newUser.style.display = "none";
-	console.log(personal)
-}
+// 	// добавления в глобальный массив
+// 	personal.push({
+// 		id: id,
+// 		firstName: firstName.value,
+// 		lastName: lastName.value,
+// 		familyName: familyName.value,
+// 		age: age.value,
+// 		profession: profession.value,
+// 		education: education.value,
+// 		institutionName: institutionName.value,
+// 		position: position.value,
+// 		wage: wage.value,
+// 		subdivision: subdivision.value,
+// 		lavel: lavel.value,
+// 		listScils : obj.scils,
+// 		listLangulares : obj.languages
+// 	})
+// }
