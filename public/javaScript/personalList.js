f254676d27d57afcb2f30b97251c96a73fc6b807
Avazkhon <Kamalxanovavazxon@gmail.come>
2019-01-23 05:02:36 +0700

    card upgrade

--- public/javaScript/personalList.js
+++ public/javaScript/personalList.js
@@ -42,19 +42,46 @@ function person(user, inner){
 function getCard(i){
   console.log(i, personals[i] )
   $("#user").html(
-    `<div>
-      <div id="getcard" ><img src="#" alt="фото"  /></div>
-      <div class="getCard" >${personals[i].id}</div>
-      <div class="getCard" >${personals[i].user.firstName}</div>
-      <div class="getCard" >${personals[i].user.lastName}</div>
-      <div class="getCard" >${personals[i].user.position}</div>
-      <div class="getCard" >${personals[i].user.lavel}</div>
-      <div class="getCard" >${personals[i].user.institutionName}</div>
-      <div class="getCard" >${personals[i].user.passUmber}</div>
-      <div class="getCard" >${personals[i].user.profession}</div>
-      <div class="getCard" >${personals[i].user.subdivision}</div>
-      <div class="getCard" >${personals[i].user.wage}</div>
+    `<div class="card">
+      <div class="getCardIMG" ><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFaORdmj-6CrbpTkWsgeTr67mI_kBy6bCF_l9WPTDL3yIC_D06" alt="фото"  /></div>
+      ${personalInformation() }
+      ${workInformation() }
+      ${education() }
     </div>`)
 
+  function personalInformation () {
+    return (
+        `<div class="personalInformation" >
+        <h3>Личная информация</h3>
+        <div class="getCardFirstName" >${personals[i].user.firstName}</div>
+        <div class="getCardLastName" >${personals[i].user.lastName}</div>
+        <div class="getCardLastName" >${personals[i].user.familyName}</div>
+      </div>`
+      )
+  }
+
+  function workInformation () {
+    return(
+        `<div class="workInformation">
+        <h3>Служебная информация</h3>
+        <div class="getCardID">${personals[i].id}</div>
+        <div class="getCardPosition" >${personals[i].user.position}</div>
+        <div class="getCardlavel" >${personals[i].user.lavel}</div>
+        <div class="getCardSubdivision" >${personals[i].user.subdivision}</div>
+        <div class="getCardWage">${personals[i].user.wage}</div>
+      </div>`
+      )
+  }
+
+  function education () {
+    return (
+     ` <div class="education" >
+        <h3>образование</h3>
+        <div class="getCardInstitutionName" >${personals[i].user.institutionName}</div>
+        <div class="getCardPassUmber" >${personals[i].user.passUmber}</div>
+        <div class="getCardProfession" >${personals[i].user.profession}</div>
+      </div>`
+    )
+  }
 }
 
